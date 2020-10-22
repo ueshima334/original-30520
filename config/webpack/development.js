986dc85e0ee10c1f3283052c4e10db579549906b
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-22 15:55:45 +0900

    jqueryの導入

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+const webpack = require('webpack') 
+ environment.plugins.append('Provide', 
+   new webpack.ProvidePlugin({ 
+     $: 'jquery', 
+     jQuery: 'jquery'
+   }) 
+ ) 
+
 module.exports = environment.toWebpackConfig()
