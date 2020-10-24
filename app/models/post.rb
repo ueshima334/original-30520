c71e74d78d197bafa9d0fc7565269415fa210f3a
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-24 17:23:47 +0900

    commentテーブルへのアソシエーションを記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
 
   has_many_attached :images,dependent: :destroy
   belongs_to :user
+  has_many :comments
 
   validates :text,        presence: { message: 'を入力してください' }
   validates :images,       presence: { message: 'を1枚以上添付してください' }  
