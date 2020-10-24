283be7cc2318754e5d9c5f6f3496c85ad5a9909c
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-24 16:18:40 +0900

    画像編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,23 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:text,images:[]).merge(user_id:current_user.id)
   end
+
 end
