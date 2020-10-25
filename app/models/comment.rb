f1baeb26d0ac76a50699a9c92b2c5268d18ae671
ueshima334 <71493889+ueshima334@users.noreply.github.com>
2020-10-25 16:43:31 +0900

    Revert "画像投稿機能の実装"

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
-
-  validates :text,length:{maximum: 50}
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,4 @@ Rails.application.routes.draw do
 root to: "users#index"
 resources :users,only:[:show]
 resources :rooms,only:[:index]
-resources :posts do
-  resources :comments,only: :create
-  get '/likes',to: 'likes#create'
-  delete '/likes',to: 'likes#destroy'
-  collection do
-    get 'search'
-  end
-end
 end
